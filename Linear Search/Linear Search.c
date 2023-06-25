b23ad4b8bb2463d081eff7ba3e200ab408ddf078
vedareydeva <vaidehi.deshmukh9@gmail.com>
2023-06-26 00:47:34 +0530

    changed values in array

--- Linear Search/Linear Search.c
+++ Linear Search/Linear Search.c
@@ -4,7 +4,7 @@
 
 int search (int arr[], int N, int x){
     for(int i = 0; i < N; i++)
-        if(arr[i] == x)
+        if(arr[i] == x)	
             return i;
     return -1;
 }
@@ -13,7 +13,7 @@ int search (int arr[], int N, int x){
 int main(void)
 {
 	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
+	int x = 4;
 	int N = sizeof(arr) / sizeof(arr[0]);
 
 	// Function call
